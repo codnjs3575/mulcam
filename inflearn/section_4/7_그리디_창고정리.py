f43051868ce84da32e7b9de913d9f5f30c0f95cc
codnjs3575 <codnjs3575@naver.com>
2022-02-07 18:06:10 +0900

    0206 update

--- inflearn/section_4/7_그리디_창고정리.py
+++ inflearn/section_4/7_그리디_창고정리.py
@@ -1,2 +1,14 @@
 import sys
 sys.stdin = open('input.txt','rt')
+
+L = int(input())
+arr = list(map(int,input().split()))
+M = int(input())
+arr.sort()
+
+for m in range(M):
+    arr[0] += 1
+    arr[-1] -= 1
+    arr.sort()
+
+print(arr[-1]-arr[0])
--- inflearn/section_4/8_그리디_타이타닉.py
+++ inflearn/section_4/8_그리디_타이타닉.py
@@ -1,2 +1,23 @@
 import sys
 sys.stdin = open('input.txt','rt')
+N, M = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+count = 0
+
+for i in range(N):
+    if len(arr) == 1:
+        count += 1
+        break
+    elif len(arr) == 0 :
+        break
+
+    if arr[-1] + arr[0] > M :
+        arr.pop()
+        count += 1
+    else :
+        arr.pop()
+        arr.pop(0)
+        count += 1
+
+print(count)
